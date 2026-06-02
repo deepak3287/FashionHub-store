38158c6f203c72b76754d8084695f472e73ba980
Deepak Sharma <deepaksharma152008@gmail.com>
2026-06-02 14:55:54 +0000

    fix firebase typescript module issue

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,9 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_ID",
   appId: "YOUR_APP_ID"
 };
 
