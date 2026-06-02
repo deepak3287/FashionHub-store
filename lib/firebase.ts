beed2ccba7ccc30fdf8cbdb01439a8f25d953b09
Deepak Sharma <deepaksharma152008@gmail.com>
2026-06-02 15:28:23 +0000

    fix firebase config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,10 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCwdkO9rXJJTr-04OzqALgJbjHKRYTN9_I",
+  authDomain: "eorstore-5c409.firebaseapp.com",
+  projectId: "eorstore-5c409",
+  storageBucket: "eorstore-5c409.appspot.com",
+  messagingSenderId: "1034124488012",
+  appId: "1:1034124488012:web:762fc0542730b2299073e9"
 };
 
 const app = initializeApp(firebaseConfig);
